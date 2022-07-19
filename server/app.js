7a5a6b036ffd6113b9e9e378c972ba7db1905169
mnvaidya <mnvaidya527@gmail.com>
2022-07-19 13:24:22 +0530

    initialize model controller structure

--- server/app.js
+++ server/app.js
@@ -2,14 +2,38 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');        // for handling requests to the external API
 
-const app = express(); 
+const eventRouters = require('./routes/event.routes')      // for event routes
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, (err)=>{
+    if(err) console.log("Error in connecting to the mongodb: ", err);
+    else{
+        console.log("Database connection formed");
+        app.listen(process.env.PORT, ()=>{
+            console.log("Server listening\nClick this link", `http://localhost:${process.env.PORT}`);
+        })
+    }
+})
+
 
 app.get('/', (req, res)=>{
-    res.send("<h2> Welcome to WebOverflow </h2>");
+    res.send('<h2> Welcome to WebOverflow! </h2>');
 })
 
 
-app.listen(process.env.PORT, ()=>{
-    console.log("Server listening\nClick this link", `http://localhost:${process.env.PORT}`);
-})
+app.use('/gallery', eventRouters)
+
+
+
+// error handling middlewares
+app.use(function(err, req, res, next){
+    
+});
