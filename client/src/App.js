394afd5f7f7c6731dfec53080d3977c96d8025e8
crimsonskies <cskies1441@gmail.com>
2022-08-16 01:49:39 +0530

    Add Upload page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./Home";
 import Gallery from "./Components/Gallery/Gallery";
 import Timeline from "./Components/Timeline/Timeline";
+import Upload from "./Components/Auth/Upload";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+export const AdminContext = React.createContext();
+
 export default function App() {
+  const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(true);
+
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery">
-            <Route path=":eventName" element={<Gallery />}>
-              <Route path=":eventYear" element={<Gallery />} />
+        <AdminContext.Provider value={{ isAdminLoggedIn, setIsAdminLoggedIn }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gallery">
+              <Route path=":eventName" element={<Gallery />}>
+                <Route path=":eventYear" element={<Gallery />} />
+              </Route>
             </Route>
-          </Route>
-          <Route path="/timeline" element={<Timeline />} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Routes>
+            <Route path="/timeline" element={<Timeline />} />
+            <Route path="/admin/upload" element={<Upload />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Routes>
+        </AdminContext.Provider>
       </BrowserRouter>
     </>
   );
